2c5440fa74ab25771a56e9397b4cdbb4a5c8273e
Charles Wolfe <cwolfe7@asu.edu>
2022-08-19 21:33:03 -0700

    grenade explodes now

--- Assets/Scripts/Projectiles/Grenade.cs
+++ Assets/Scripts/Projectiles/Grenade.cs
@@ -6,9 +6,16 @@ public class Grenade : MonoBehaviour
 {
     [SerializeField] float velocityX = 5f;
     [SerializeField] float velocityY = 5f;
+    [SerializeField] float explosionDelayMin = 4f;
+    [SerializeField] float explosionDelayMax = 5f;
+    [SerializeField] float explosionDuration = 0.5f;
+    [SerializeField] GameObject explosion;
+    [SerializeField] float explosionOffsetX = 0.42f;
+    [SerializeField] float explosionOffsetY = 2.04f;
 
     Rigidbody2D rigidbody;
     float orientation;
+    AudioSource explosionSound;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +23,37 @@ public class Grenade : MonoBehaviour
         orientation = GetComponentInParent<OrientationTracker>().GetOrientation();
         rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.velocity = new Vector2(velocityX * orientation, velocityY);
+        explosionSound = GetComponent<AudioSource>();
 
         // de-child grenade from thrower so it does not move with thrower
         transform.parent = null;
+
+        float explosionDelay = Random.Range(explosionDelayMin, explosionDelayMax);
+        StartCoroutine(ExplodeCoroutine(explosionDelay));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator ExplodeCoroutine(float explosionDelay)
+    {
+        yield return new WaitForSeconds(explosionDelay);
+
+        // make grenade disappear
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        // show explosion in place of grenade and play sound
+        explosionSound.Play();
+        Vector3 explosionPoint = new Vector3(transform.position.x + explosionOffsetX, transform.position.y + explosionOffsetY, transform.position.z);
+        GameObject grenadeExplosion = Instantiate(explosion, explosionPoint, explosion.transform.rotation);
+        
+        // remove grenade and explosion
+        Destroy(grenadeExplosion, explosionDuration);
+        Destroy(gameObject, explosionDuration);
+
     }
 
 }
