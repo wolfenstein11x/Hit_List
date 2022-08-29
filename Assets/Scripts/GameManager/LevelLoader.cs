86b4c56ac543b5238ce8109862f8321c8b2a33a6
Charles Wolfe <cwolfe7@asu.edu>
2022-08-28 23:33:10 -0700

    added music to main menu

--- Assets/Scripts/GameManager/LevelLoader.cs
+++ Assets/Scripts/GameManager/LevelLoader.cs
@@ -27,4 +27,9 @@ public class LevelLoader : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
 }
