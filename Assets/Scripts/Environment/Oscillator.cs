5e9c1f9b6d8e0f35fe96c28823bffb75844f52f5
Charles Wolfe <cwolfe7@asu.edu>
2022-09-07 19:03:50 -0700

    added oscillator platform

---
+++ Assets/Scripts/Environment/Oscillator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVec;
+    [SerializeField] [Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    Vector3 startingPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSineWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSineWave + 1f) / 2f;
+
+        Vector3 offset = movementVec * movementFactor;
+
+        transform.position = startingPos + offset;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+}
