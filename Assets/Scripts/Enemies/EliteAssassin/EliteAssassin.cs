beafe0bb0193543bdd134b366f7a82b487fd9d82
Charles Wolfe <cwolfe7@asu.edu>
2022-09-13 18:55:30 -0700

    elite assassin turns to face player

--- Assets/Scripts/Enemies/EliteAssassin/EliteAssassin.cs
+++ Assets/Scripts/Enemies/EliteAssassin/EliteAssassin.cs
@@ -6,6 +6,11 @@ public class EliteAssassin : Enemy
 {
     [SerializeField] GameObject grenade;
     [SerializeField] Transform grenadeSpawnPoint;
+    [SerializeField] float sightRange = 100;
+
+    public bool grenadeThrower = true;
+
+    PlayerMovement target;
 
     void Start()
     {
@@ -17,6 +22,7 @@ public class EliteAssassin : Enemy
         orientationTracker = GetComponent<OrientationTracker>();
         raycastLayers = LayerMask.GetMask("Player") | LayerMask.GetMask("Ground");
         bodyCollider = GetComponent<Collider2D>();
+        target = FindObjectOfType<PlayerMovement>();
     }
 
     public void ThrowGrenade()
@@ -24,4 +30,31 @@ public class EliteAssassin : Enemy
         GameObject liveGrenade = Instantiate(grenade, grenadeSpawnPoint.position, grenade.transform.rotation);
         liveGrenade.transform.parent = gameObject.transform;
     }
+
+    // 0 = not spotted, 1 = spotted in front, 2 = spotted behind
+    public bool TargetSpotted(int direction=1)
+    {
+        float orientation = orientationTracker.GetOrientation();
+
+        RaycastHit2D hit = Physics2D.Raycast(gunmanTransform.position, Vector2.right * new Vector2(direction*orientation, 0f), sightRange, raycastLayers);
+
+        //Debug.DrawRay(gunmanTransform.position, Vector2.right * hit.distance * new Vector2(direction*orientation, 0f), Color.red);
+
+        if (!hit) { return false; }
+
+        if (hit.collider.gameObject.tag == "Player")
+        {
+            //Debug.DrawRay(gunmanTransform.position, Vector2.right * hit.distance * new Vector2(orientation, 0f), Color.red);
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    
 }
